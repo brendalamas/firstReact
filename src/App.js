2a98681c1452029b22b365e6e8876e48cd865d49
brenda <lamasbrenda@hotmail.com>
2022-01-19 21:46:23 -0300

    primera react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Card";
+import CardContainer from "./CardContainer";
+import "./App.css";
 
-function App() {
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = ()=>{
+  const nombre = "brenda"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="contenedor">
+
+      <h1>Hola {nombre} </h1>
+      <CardContainer></CardContainer>
+      <CardContainer></CardContainer>
+
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,10 @@
+const Card = ()=>{
+    return(
+        <article className="tarjeta">
+            <h3>Soy un gatito</h3>
+            <img src= "http://placekitten.com/200"></img>
+        </article>
+    )
+}
+
+export default Card;
